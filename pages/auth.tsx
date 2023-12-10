61edccd9b3d1bb3bd20109a40425bd5146bdda2e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-10 21:49:19 +0200

    Auth UI

---
+++ pages/auth.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
+import Image from 'next/image'
+import React from 'react'
+
+export default function auth() {
+  return (
+    <div className="relative h-full w-full bg-no-repeat bg-center bg-fixed bg-cover bg-[url('/images/hero.jpg')]">
+        <div className='bg-black w-full h-full lg:bg-opacity-70'>
+            <nav className='px-12 py-5'>
+                <Image src="/images/logo.png" width={100} height={48} alt="Logo"/>
+            </nav>
+            <div className='flex justify-center'>
+                <div className='bg-black bg-opacity-80 px-16 py-16 self-center mt-2 lg:max-w-md rounded-md w-full'>
+                    <h2 className='text-white text-4xl mb-8 font-semibold'>Sign in</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="font-bold">Netflix clone</h1>
+  return <h1 className="font-bold text-white">Netflix clone</h1>
 }
