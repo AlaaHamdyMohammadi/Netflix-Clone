860c99684774b149c98dc912e9e647c3b12f7d93
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-10 23:52:22 +0200

    Auth UI

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -18,17 +18,17 @@ export default function auth() {
   
 
   return (
-    <div className="relative h-full w-full bg-no-repeat bg-center bg-fixed bg-cover bg-[url('/images/hero.jpg')]">
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-70">
         <nav className="px-12 py-5">
           <Image src="/images/logo.png" width={100} height={48} alt="Logo" />
         </nav>
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-80 px-16 py-16 self-center mt-2 lg:max-w-md rounded-md w-full">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
               {varient === "login" ? "Sign in" : "Register"}
             </h2>
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-3">
               {varient === "register" && (
                 <Input
                   id="username"
@@ -56,13 +56,15 @@ export default function auth() {
             <button className="bg-red-600 hover:bg-red-700 py-3 px-3 text-white rounded-md w-full mt-10 transition">
               {varient === "login" ? "Login" : "Sign up"}
             </button>
-            <p className="text-neutral-500 mt-12">
-              {varient === 'login' ? 'First time using Netflix?' : 'Already have an account'}
+            <p className="text-neutral-500 mt-10">
+              {varient === "login"
+                ? "First time using Netflix?"
+                : "Already have an account"}
               <span
                 onClick={toggleVarient}
                 className="text-white ml-1 hover:underline cursor-pointer"
               >
-                {varient === 'login' ? 'Create an account' : 'Login'}
+                {varient === "login" ? "Create an account" : "Login"}
               </span>
             </p>
           </div>
