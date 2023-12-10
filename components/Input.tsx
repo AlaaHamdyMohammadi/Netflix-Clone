64607268b996e55c673d57073d94384c5e814122
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-10 23:46:54 +0200

    Auth UI

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React from 'react';
 
-export default function Input() {
+interface InputProps{
+    id: string,
+    onChange: any,
+    value: string,
+    label: string,
+    type?: string
+}
+
+const Input: React.FC<InputProps> = ({
+    id, onChange, value, label, type
+}) =>{
   return (
     <div className="relative">
       <input
-        id="email"
+        id={id}
         className="rounded-md px-6 py-6 pb-1 w-full  text-md bg-neutral-700 text-white block focus:outline-none focus:ring-0 appearance-none peer"
-        type="email"
+        type={type}
         placeholder=" "
+        value={value}
+        onChange={onChange}
       />
       <label
-        htmlFor="email"
+        htmlFor={id}
         className="absolute text-zinc-400 text-md transform duration-150 -translate-y-3 scale-75 top-4 left-6 z-10 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3 origin-[0]"
       >
-        Email
+        {label}
       </label>
     </div>
   );
 }
+
+export default Input;
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,24 +1,73 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable jsx-a11y/alt-text */
-import Input from '@/components/Input'
-import Image from 'next/image'
-import React from 'react'
+
+import React from "react";
+import { useState, useCallback } from "react";
+import Image from "next/image";
+import Input from "@/components/Input";
 
 export default function auth() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [varient, setVarient] = useState('login');
+
+  const toggleVarient = useCallback(() => {
+    setVarient((currentVarient) => currentVarient === 'login' ? 'register' : 'login');
+  }, []);
+  
+
   return (
     <div className="relative h-full w-full bg-no-repeat bg-center bg-fixed bg-cover bg-[url('/images/hero.jpg')]">
-        <div className='bg-black w-full h-full lg:bg-opacity-70'>
-            <nav className='px-12 py-5'>
-                <Image src="/images/logo.png" width={100} height={48} alt="Logo"/>
-            </nav>
-            <div className='flex justify-center'>
-                <div className='bg-black bg-opacity-80 px-16 py-16 self-center mt-2 lg:max-w-md rounded-md w-full'>
-                    <h2 className='text-white text-4xl mb-8 font-semibold'>Sign in</h2>
-                    <div className='flex flex-col gap-4'>
-                        <Input/>
-                    </div>
-                </div>
+      <div className="bg-black w-full h-full lg:bg-opacity-70">
+        <nav className="px-12 py-5">
+          <Image src="/images/logo.png" width={100} height={48} alt="Logo" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-80 px-16 py-16 self-center mt-2 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {varient === "login" ? "Sign in" : "Register"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {varient === "register" && (
+                <Input
+                  id="username"
+                  label="Username"
+                  type="text"
+                  value={username}
+                  onChange={(e: any) => setUsername(e.target.value)}
+                />
+              )}
+              <Input
+                id="email"
+                label="Email"
+                type="email"
+                value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
+              />
+              <Input
+                id="password"
+                label="Password"
+                type="password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
+              />
             </div>
+            <button className="bg-red-600 hover:bg-red-700 py-3 px-3 text-white rounded-md w-full mt-10 transition">
+              {varient === "login" ? "Login" : "Sign up"}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {varient === 'login' ? 'First time using Netflix?' : 'Already have an account'}
+              <span
+                onClick={toggleVarient}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {varient === 'login' ? 'Create an account' : 'Login'}
+              </span>
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
